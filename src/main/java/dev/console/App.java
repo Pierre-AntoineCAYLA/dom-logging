e37327c9f5a69e598d34ffed7f1f30c73f667914
Pierre-Antoine <you@example.com>
2018-04-24 11:19:11 +0200

    creation package dev.service

--- src/main/java/dev/console/App.java
+++ src/main/java/dev/console/App.java
@@ -3,6 +3,8 @@ package dev.console;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import dev.service.AppService;
+
 public class App 
 {
 	private static final Logger LOG = LoggerFactory.getLogger(App.class);
